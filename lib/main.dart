363da156c938ccdbeec56ff02ed551ece6c8d071
Kabbo <kabbo1997@gmail.com>
2022-08-14 19:54:47 +0600

    Container added with BG Color

--- lib/main.dart
+++ lib/main.dart
@@ -24,31 +24,39 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             if (_response != null)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        '${_response?.temp.toInt()}°C',
-                        style: TextStyle(fontSize: 40),
-                      ),
-                      Text(
-                        'feels like ${_response?.feelsLike.toInt()}°C',
-                        style: TextStyle(fontSize: 14),
-                      ),
-                      Text(
-                        '${_response?.description}',
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                            backgroundColor: Colors.green),
-                      ),
-                    ],
-                  ),
-                  Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
-                  Image.network(_response!.inconUrl),
-                ],
+              Container(
+                margin: const EdgeInsets.only(left: 40, right: 40),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black26,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          '${_response?.temp.toInt()}°C',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        Text(
+                          'feels like ${_response?.feelsLike.toInt()}°C',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        Text(
+                          '${_response?.description.toTitleCase()}',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              backgroundColor: Colors.green),
+                        ),
+                      ],
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+                    Image.network(_response!.inconUrl),
+                  ],
+                ),
               ),
             Padding(padding: EdgeInsets.symmetric(vertical: 20)),
             SizedBox(
@@ -73,4 +81,13 @@ class _MyAppState extends State<MyApp> {
     final response = await _dataService.getWeather(controller.text);
     setState(() => _response = response);
   }
+}
+
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
+      .split(' ')
+      .map((str) => str.toCapitalized())
+      .join(' ');
 }
